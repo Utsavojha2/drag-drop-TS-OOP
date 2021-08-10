331fd8a0651045e9291b2947a71887606d454dfd
Utsav Ojha <utsavojha2@gmail.com>
2021-08-10 23:16:01 +0545

    :wq
    Revert "Revert "d and d proj with typescript""
    
    This reverts commit 369376aafa29287b7fcbe90654a56b9205704831.

---
+++ dist/helpers.js
@@ -0,0 +1,5 @@
+export const icons = {
+    edit: 'https://img.icons8.com/material-outlined/22/000000/edit--v1.png',
+    close: 'https://img.icons8.com/ios-glyphs/25/000000/macos-close.png',
+    pin: 'https://img.icons8.com/ios-glyphs/25/000000/pin3--v1.png',
+};
